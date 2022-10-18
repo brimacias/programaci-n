e7996c61fae8c052684e754c7a1d59a102674c7e
brimacias <95747897+brimacias@users.noreply.github.com>
2022-10-18 21:09:02 +0200

    Delete mostrar1al100.java

--- mostrar1al100.java
+++
@@ -1,9 +0,0 @@
-public class mostrar1al100 {
-    public static void main(String[] args) {
-        int num = 0;
-        while (num >= 0 && num <= 99) {
-            num++;
-            System.out.println(num);
-        }
-    }
-}
