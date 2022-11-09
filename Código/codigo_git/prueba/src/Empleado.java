1fb62e26f68eb7040a4adc69d767ef5f6dae6e3f
brimacias <95747897+brimacias@users.noreply.github.com>
2022-11-09 20:40:19 +0100

    commit después de hacer el ejercicio PagoOrdenador

--- Código/codigo_git/prueba/src/Empleado.java
+++ Código/codigo_git/prueba/src/Empleado.java
@@ -21,63 +21,42 @@ public class Empleado {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public float getSueldo() {
         return sueldo;
     }
-
-    public void setSueldo(float sueldo) {
-        this.sueldo = sueldo;
+    public float getDescuento21(){
+        return 21;
     }
 
-    public float getDescuento() {
-        return descuento;
-    }
-
-    public void setDescuento(float descuento) {
-        this.descuento = 21;
+    public float getDescuento(){
+        return porcentaje*(sueldo+numHoras/100);
     }
 
     public byte getNumHorasExtras() {
         return numHoras;
     }
 
-    public void setNumHoras(byte numHoras) {
-        this.numHoras = numHoras;
-    }
-
     public float getPrecioHora() {
         return precioHora;
     }
 
-    public void setPrecioHora(float precioHora) {
-        this.precioHora = precioHora;
+    public float getPrecioHorasExtras(){
+        return numHoras*precioHora;
     }
 
     public String getMes() {
         return mes;
     }
 
-    public void setMes(String mes) {
-        this.mes = mes;
-    }
-
     public int getPorcentaje() {
         return porcentaje;
     }
 
-    public void setPorcentaje(int porcentaje) {
-        this.porcentaje = porcentaje;
-    }
-
     public float getDiferencia() {
-        return diferencia;
+        return sueldo-descuento;
     }
 
-    public void setDiferencia(float diferencia) {
-        this.diferencia = sueldo-descuento;
+    public float getSueldoNeto(){
+        return diferencia+numHoras;
     }
 }
--- Código/codigo_git/prueba/src/principalEmpleado.java
+++ Código/codigo_git/prueba/src/principalEmpleado.java
@@ -4,13 +4,13 @@ public class principalEmpleado {
     public static void main(String[] args) {
         Empleado e1 = new Empleado("Agus",(float) 1600.5, (byte) 12,"octubre");
 
-        System.out.println("Mes: " + e1.getMes());
-        System.out.println("Nombre del empleado: "  + e1.getNombre());
-        System.out.println("Sueldo Base: " + e1.getSueldo());
-        System.out.println("Porcentaje de descuento: " + e1.getDescuento());
-        System.out.println("HorasExtra: " + (e1.getNumHorasExtras()*e1.getPrecioHora()));
-        System.out.println("Descuentos: " + (e1.getPorcentaje()*(e1.getSueldo() + e1.getNumHorasExtras())/100));
-        System.out.println("Diferencia: " + (e1.getDiferencia()));
-        System.out.println("Sueldo neto: " + (e1.getDiferencia()+e1.getNumHorasExtras()));
+        System.out.println("Mes: " + e1.getMes() +
+                "\nNombre del empleado: "  + e1.getNombre() +
+                "\nSueldo Base: " + e1.getSueldo() +
+                "\nPorcentaje de descuento: " + e1.getDescuento21() +
+                "\nHorasExtra: " + e1.getPrecioHorasExtras() +
+                "\nDescuentos: " + e1.getDescuento() +
+                "\nDiferencia: " + e1.getDiferencia() +
+                "\nSueldo neto: " + e1.getSueldoNeto());
     }
 }
