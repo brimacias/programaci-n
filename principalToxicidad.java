e616d10475911a8fe2ee603d9edffeb4bb255ae7
brimacias <95747897+brimacias@users.noreply.github.com>
2023-01-31 21:42:51 +0100

    Delete principalToxicidad.java

--- principalToxicidad.java
+++
@@ -1,7 +0,0 @@
-public class principalToxicidad {
-    public static void main(String[] args) {
-        Toxicidad t1 = new Toxicidad("Producto toxico",5);
-        System.out.println("La toxicidad del producto " + Toxicidad.getNombreProducto()
-                + " es " + Toxicidad.getGradoToxicidad() + ".");
-    }
-}
