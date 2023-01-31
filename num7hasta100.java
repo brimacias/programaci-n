d87edf263eb55f53b0718bee2f1097198ba0a18f
brimacias <95747897+brimacias@users.noreply.github.com>
2023-01-31 21:41:58 +0100

    Delete num7hasta100.java

--- num7hasta100.java
+++
@@ -1,8 +0,0 @@
-public class num7hasta100 {
-    public static void main(String[] args) {
-        int num = 0;
-        for(int i = 0; i < 100; i++) {
-            num = num + 7;
-        }
-    }
-}
